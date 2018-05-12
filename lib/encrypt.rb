1e3aa8433d66a2eee442cabf8c43ce8ef917920a
Angela Duran <duranangela19@gmail.com>
2018-05-11 22:24:10 -0600

    Add in rotations and stuff

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -1,16 +1,59 @@
+require './lib/rotation'
+
 class Encrypt
   attr_reader :msg,
               :key,
-              :date
+              :date,
+              :char_map
 
   def initialize(msg, key, date)
-    @msg = msg
+    @msg = msg.split(//)
     @key = key.rjust(5, '0')
     @date = date.to_s
+    @char_map = "abcdefghijklmnopqrstuvwxyz0123456789 .,".split(//)
+  end
+
+  def a_rotation
+    key[0..1].to_i + offset[0].to_i
+  end
+
+  def b_rotation
+    key[1..2].to_i + offset[1].to_i
+  end
+
+  def c_rotation
+    key[2..3].to_i + offset[2].to_i
+  end
+
+  def d_rotation
+    key[3..4].to_i + offset[3].to_i
+  end
+
+  def offset
+    date_code = (@date.to_i ** 2).to_s
+    date_code[-4..-1]
   end
 
   def output
-    msg
+    r = 0
+    enc_msg = []
+    msg.each do |char|
+      if r == 4
+        r = 0
+      elsif r == 0
+        rot = a_rotation
+      elsif r == 1
+        rot = b_rotation
+      elsif r == 2
+        rot = c_rotation
+      elsif r == 3
+        rot = d_rotation
+      end
+      enc_char = char_map[msg.index(char) + rot]
+      enc_msg << enc_char
+      r += 1
+    end
+    enc_msg.join
   end
 
 end
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -9,6 +9,16 @@ class EnigmaTest < Minitest::Test
     assert_instance_of Enigma, e
   end
 
+  def test_it_makes_a_five_digit_key
+    e = Enigma.new
+    assert_equal 5, e.random_key.length
+  end
+
+  def test_it_gives_todays_date
+    e = Enigma.new
+    assert_equal Date.today.strftime('%m%e%y'), e.today
+  end 
+
   def test_it_encrypts
     e = Enigma.new
     assert_equal 'msg', e.encrypt('msg')
