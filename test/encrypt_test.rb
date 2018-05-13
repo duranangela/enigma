e463dbd7b6e5c6ebca75e2bf7274ccde736e5c12
Blythe McFall <mcfall.blythe@gmail.com>
2018-05-13 08:02:09 -0600

    Altered naming convention from msg to my_message; Added test for message and date argument only; Added test to confirm rotation methods

--- test/encrypt_test.rb
+++ test/encrypt_test.rb
@@ -5,17 +5,38 @@ require './lib/encrypt'
 class EncryptTest < Minitest::Test
 
   def test_it_exists
-    encrypted = Encrypt.new('msg', '12345', '051118')
-    assert_instance_of Encrypt, encrypted
+    encrypt = Encrypt.new('my_message', '12345', '051318')
+    assert_instance_of Encrypt, encrypt
   end
 
   def test_it_has_attributes
-    encrypted = Encrypt.new('msg', '12345', '051118')
-    assert_equal 'msg', encrypted.msg
-    assert_equal '12345', encrypted.key
-    assert_equal '051118', encrypted.date
-    assert_equal "abcdefghijklmnopqrstuvwxyz0123456789 .,".split(//), encrypted.map
+    encrypt = Encrypt.new('my_message', '12345', '051318')
+    assert_equal ["m", "y", "_", "m", "e", "s", "s", "a", "g", "e"], encrypt.my_message
+    assert_equal '12345', encrypt.key
+    assert_equal '051318', encrypt.date
+    assert_equal "abcdefghijklmnopqrstuvwxyz0123456789 .,".split(//), encrypt.char_map
   end
 
+  def test_only_message_argument_needed
+    encrypt = Encrypt.new('my_message')
+
+    assert_equal ["m", "y", "_", "m", "e", "s", "s", "a", "g", "e"], encrypt.my_message
+  end
+
+  def test_only_message_and_date_argument_needed
+    encrypt = Encrypt.new('my_message', '020315')
+
+    assert_equal ["m", "y", "_", "m", "e", "s", "s", "a", "g", "e"], encrypt.my_message
+    assert_equal '020315', encrypt.date
+  end
+
+  def test_the_a__b_c_d_of_rotations
+    encrypt = Encrypt.new('my_message', '12345', '020315')
+
+    assert_equal 21, encrypt.a_rotation
+    assert_equal 25, encrypt.b_rotation
+    assert_equal 36, encrypt.c_rotation
+    assert_equal 50, encrypt.d_rotation
+  end
 
 end
