a75014f49615eaf85df0b5f2d4a6eed811fe2e35
Angela Duran <duranangela19@gmail.com>
2018-05-16 17:52:30 -0600

    Formatted some more stuff

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -1,14 +1,14 @@
 require './lib/enigma'
 
-handle = File.open(ARGV[0], 'r')
-incoming_text = handle.read.chomp
-handle.close
+file = File.open(ARGV[0], 'r')
+incoming_text = file.read.chomp
+file.close
 
 e = Enigma.new
 output = e.decrypt(incoming_text, ARGV[2], ARGV[3])
 
-writer = File.open(ARGV[1], 'w')
-writer.write(output)
-writer.close
+new_file = File.open(ARGV[1], 'w')
+new_file.write(output)
+new_file.close
 
 puts "Created 'decrypted.txt' with the key #{e.key} and date #{e.decrypt_date}"
--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -1,14 +1,14 @@
 require './lib/enigma'
 
-handle = File.open(ARGV[0], 'r')
-incoming_text = handle.read.chomp
-handle.close
+file = File.open(ARGV[0], 'r')
+incoming_text = file.read.chomp
+file.close
 
 e = Enigma.new
 output = e.encrypt(incoming_text)
 
-writer = File.open(ARGV[1], 'w')
-writer.write(output)
-writer.close
+new_file = File.open(ARGV[1], 'w')
+new_file.write(output)
+new_file.close
 
 puts "Created 'encrypted.txt' with the key #{e.key} and date #{e.date}"
