d03aadb04317232111f4b0438a2d48a3b6e3ae9c
Angela Duran <duranangela19@gmail.com>
2018-05-16 18:14:07 -0600

    Added extra characters to charmap

--- lib/charmap.rb
+++ lib/charmap.rb
@@ -1,10 +1,11 @@
 class CharMap
 
   def charmap
-    lower_case = ("a".."z").to_a
+    upper_case = ('A'..'Z').to_a
+    lower_case = ('a'..'z').to_a
     numbers = (0..9).to_a.join.chars
-    extra_characters = ' .,'.split(//)
-    lower_case + numbers + extra_characters
+    extra_characters = ' !@#$%^&*()[],.<>;:/?\|'.split(//)
+    upper_case + lower_case + numbers + extra_characters
   end
 
 end
